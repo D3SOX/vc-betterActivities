69ecf991a079f5b235088d15d860ee4c04edd904
thororen1234 <78185467+thororen1234@users.noreply.github.com>
2025-03-22 01:16:58 -0400

    Fix UserProfilePopout

--- index.tsx
+++ index.tsx
@@ -474,10 +474,10 @@ export default definePlugin({
         {
             // Show all activities in the user popout/sidebar
             // still broken btw
-            find: '"UserActivityContainer"',
+            find: '"UserProfilePopoutBody"',
             replacement: {
-                match: /(?<=\(0,\i\.jsx\)\()(\i\.\i)(?=,{...(\i),activity:\i,user:\i,application:\i)/,
-                replace: "$2.type==='BiteSizePopout'?$self.showAllActivitiesComponent:$1"
+                match: /(?<=user:(\i).*?\i\.id\)\}\)\),(\i).*?)\(0,\i\.jsx\).{0,100}\i\.activity\}\)/,
+                replace: ",$self.showAllActivitiesComponent({ activity: U, user: $1 })"
             },
             predicate: () => settings.store.userPopout
         },
